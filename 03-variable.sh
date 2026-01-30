2cbf19c68aa83f9dd8b76866f31fbe66e992f797
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-01-30 18:55:46 +0530

    shell-practice

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "printinh timestamp"
+# syntax VAR_NAME=$(command) ,,,this queston asks in interview 
 TIMESTAMP=$(date)
-echo "script executed in $TIMESTAMP"
+echo "script executed in $TIMESTAMP"  
+
+sleep 10
+
+ENDTIME =$(date)
+echo " end time is $ENDTIME "
