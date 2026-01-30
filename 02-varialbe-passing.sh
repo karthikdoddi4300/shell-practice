60b49273eb50498f0f9704ec6f095dffc5324249
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-01-30 18:23:44 +0530

    shell-practice

--- 02-varialbe-passing.sh
+++ 02-varialbe-passing.sh
@@ -3,5 +3,5 @@ echo "enter your name"
 read USERNAME
 echo " ur name is $USERNAME"
 echo " enter your password"
-read PASSWD
+read -s PASSWD # -s hides the passwd ,it is very useful
 echo " ur passwd is $PASSWD"
