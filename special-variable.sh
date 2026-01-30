d58c827bc7c3a314ad6346b526c052231b3415ad
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-01-30 18:11:20 +0530

    shell-practice

---
+++ special-variable.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1 : hello , $person2"
+echo "$peron2:hi ,how is u doing"
+echo "$person1: doing well, what about u "
+echo "$person2: i am good"
+
