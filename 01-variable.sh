a7ad02c2aba76cabf08c01bcde3e6a46d116fcf6
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-01-30 17:52:33 +0530

    shell-practice

---
+++ 01-variable.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+person1=karthik
+person2=chandu
+
+echo"$person1 : hello , $person2"
+echo"$peron2:hi ,how is u doing"
+echo"$person1: doing well, what about u "
+echo"$person2: i am good"
+
