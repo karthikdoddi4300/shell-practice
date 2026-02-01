26225f8d8346196a5ccc4e09770cc665612c8995
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-01 14:38:20 +0530

    shell-practice

--- 01-logs.sh
+++ 01-logs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #while installin a software ,we have to be in root user
 
-LOGS_FOLDER="/var/log/shell-practice"
-LOGS_FILE="/var/log/shell-practice/$0.log"
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 userid=$(id -u)
 if [ $userid -ne 0  ]; then 
 echo " please run this script with root access"
@@ -19,8 +19,8 @@ VALIDATE(){
     fi
 }
 
-dnf install nginx -y  &>> $LOGS_FOLDER # $? checks the exit code of the nginx and pass to the validate fun 
+dnf install nginx -y  &>> $LOGS_FILE # $? checks the exit code of the nginx and pass to the validate fun 
 VALIDATE $? "INSTALLING NGINX" # here we are passing two arguments to the VALIDATE function  that are $? and "installing nginx"
 
-dnf install nodejs -y &>> $LOGS_FOLDER
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "INSTALLING NODEJS"
