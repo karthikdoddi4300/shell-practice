671e8360c0462058005be6097d26df7972f8db11
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-01 10:14:17 +0530

    shell-practice

--- 01-conditions.sh
+++ 01-conditions.sh
@@ -2,8 +2,8 @@
 echo " conditions in shell "
 num1=100
 num2=200
-if [($num1 -gt $num2 )]; then 
-    echo " $num1 is greater than $num2 "
+if [ $num1 -gt $num2 ] ; then 
+    echo "$num1 is greater than $num2 "
 else
-    echo " $num2 is greater than $num1"
+    echo "$num2 is greater than $num1"
 fi 
