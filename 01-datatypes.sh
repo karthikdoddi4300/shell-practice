94a5766ea74ec04cf93b9df539999540c036c474
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-01 10:02:30 +0530

    shell-practice

--- 01-datatypes.sh
+++ 01-datatypes.sh
@@ -2,5 +2,5 @@
 echo " data types in shell "
 fruits=("apple" "banana" "mango")
 echo "fruits are : ${fruits[@]}"
-echo " second fruits is : ${fruits[1]}
+echo " second fruits is : ${fruits[1]}"
 # in shell there are two data types ,numbers and lists or arrays or strings all same s
