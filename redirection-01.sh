81d08aebfa900efc799d57978c6c2acb1024f8fd
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-01 14:19:25 +0530

    shell-practice

---
+++ redirection-01.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+echo " hey this is karthik "
+echo "this is Redirection in shell scripting"
+ls -l > output.txt 
+lsdf -l >  output.txt
+ls -l 1> output.txt 
+lssdf -l 2> output.txt 
