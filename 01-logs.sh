5a0b7760a04257d1c1255ead71a1e87639fe0be9
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-01 14:30:29 +0530

    shell-practice

---
+++ 01-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#while installin a software ,we have to be in root user
+
+LOGS_FOLDER="/var/log/shell-practice"
+LOGS_FILE="/var/log/shell-practice/$0.log"
+userid=$(id -u)
+if [ $userid -ne 0  ]; then 
+echo " please run this script with root access"
+exit 1
+fi
+mkdir -p LOGS_FOLDER
+
+VALIDATE(){
+    if [  $1 -ne 0 ]; then  
+        echo " $2  ....is failure"
+        exit 1
+    else 
+        echo "$2  .....is success"
+    fi
+}
+
+dnf install nginx -y  &>> $LOGS_FOLDER # $? checks the exit code of the nginx and pass to the validate fun 
+VALIDATE $? "INSTALLING NGINX" # here we are passing two arguments to the VALIDATE function  that are $? and "installing nginx"
+
+dnf install nodejs -y &>> $LOGS_FOLDER
+VALIDATE $? "INSTALLING NODEJS"
--- redirection-01.sh
+++ redirection-01.sh
@@ -5,3 +5,7 @@ ls -l > output.txt
 lsdf -l >  output.txt
 ls -l 1> output.txt 
 lssdf -l 2> output.txt 
+ls -l &> output.txt # it is for both success or fail 
+
+
+# this script will not execute it is for note purpose
